3d4ade177a685d2db3a9fc4f65538894458af459
HOU <cz_hou@outlook.com>
2018-03-12 12:36:15 +0800

    update data path

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 mkdir data checkpoints 
 cd data
-wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+wget http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat
 wget http://images.cocodataset.org/zips/train2014.zip
-7z x train2014.zip
+unzip train2014.zip
 
 mkdir style_targets
 wget http://bpic.588ku.com/element_origin_min_pic/16/12/22/c903ceed25128f30592f101717ef656f.jpg
