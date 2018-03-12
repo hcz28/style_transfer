73d83a36e2888e552977b80dacf78ddc7ce42cd0
HOU <cz_hou@outlook.com>
2018-03-12 19:12:07 +0800

    correct errors

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
 mkdir data checkpoints 
-cd data
-wget http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat
-wget http://images.cocodataset.org/zips/train2014.zip
-unzip train2014.zip
+wget http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat -P data/
+wget http://images.cocodataset.org/zips/train2014.zip -P data/
+unzip data/train2014.zip -d data/
 
-mkdir style_targets
-wget http://bpic.588ku.com/element_origin_min_pic/16/12/22/c903ceed25128f30592f101717ef656f.jpg
-mv *.jpg plum_blossom.jpg
+mkdir data/style_targets
+wget
+http://bpic.588ku.com/element_origin_min_pic/16/12/22/c903ceed25128f30592f101717ef656f.jpg
+-P data/style_targets
 
-python style.py --style /data/style_targets/plum_blossom.jpg
+mv data/style_targets/*.jpg data/style_targets/plum_blossom.jpg
+
+python style.py --style data/style_targets/plum_blossom.jpg
